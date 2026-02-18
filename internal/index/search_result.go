1388671fcf956151f13603ac8260a28e6753cd5d
Kaustubh <kaustubh14kasbe2000@gmail.com>
2026-02-18 17:04:13 +0530

    refactor : made struct fields public

--- internal/index/search_result.go
+++ internal/index/search_result.go
@@ -2,13 +2,13 @@ package index
 
 // SearchResult represent onematch result after vector serach, immutable, ordered by desceding similarity score
 type SearchResult struct {
-	vecId string
-	score float64
+	VecId int
+	Score float64
 }
 
-func (r SearchResult) ID() string {
-	return r.vecId
-}
-func (r SearchResult) Score() float64 {
-	return r.score
-}
+// func (r SearchResult) ID() int {
+// 	return r.vecId
+// }
+// func (r SearchResult) Score() float64 {
+// 	return r.score
+// }
