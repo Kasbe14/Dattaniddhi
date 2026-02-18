4e4ddeda32de683d313d6e051e25e0258bcdd171
Kaustubh <kaustubh14kasbe2000@gmail.com>
2026-02-18 16:56:37 +0530

    refactor : changed function signature types

--- internal/index/index.go
+++ internal/index/index.go
@@ -5,9 +5,9 @@ import (
 )
 
 type VectorIndex interface {
-	Add(id string, v *v.Vector) (bool, error)
-	Delete(id string) error
-	Get(id string) (*v.Vector, bool)
+	Add(id int, v *v.Vector) (bool, error)
+	Delete(id int) error
+	Get(id int) (*v.Vector, bool)
 	Search(query *v.Vector, k int) ([]SearchResult, error)
 	Size() int
 }
