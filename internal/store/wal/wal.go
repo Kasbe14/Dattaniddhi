a2a4a855196349424af1938b608da841ca8a4920
Kaustubh <kaustubh14kasbe2000@gmail.com>
2026-02-25 18:16:28 +0530

    Implement : WAL manager + construtor + getLatestSegmentID + unit tests pending

--- internal/store/wal/wal.go
+++ internal/store/wal/wal.go
@@ -1,5 +1,21 @@
 package wal
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type SyncPolicy int
+
+const (
+	SyncEverySec SyncPolicy = iota + 1
+	SyncAlways
+	SyncOS
+)
+
 const (
 	// Magic number
 	magicBytes = "SANGITA"
@@ -10,4 +26,98 @@ const (
 	OpInsert uint8 = 1
 	OpDelete uint8 = 2
 	OpUpdate uint8 = 3
+	//Version
+	walVersion uint8 = 1
 )
+
+type WAL struct {
+	mu            sync.Mutex
+	dir           string
+	activeSegment *segment
+	lsn           uint64
+	segID         uint64
+	syncPolicy    SyncPolicy
+}
+
+func NewWAL(dir string, sp SyncPolicy) (*WAL, error) {
+	if sp < 1 || sp > 3 {
+		return nil, fmt.Errorf("invalid sync policy %d", sp)
+	}
+	//directroy scanning for latest segment
+	segID, found, err := getLatestSegmentID(dir)
+	if err != nil {
+		return nil, err
+	}
+	var activeSeg *segment
+	var lsn uint64
+	switch found {
+	case true:
+		//open latest segment file
+		activeSeg, err = openExistingSegment(dir, segID)
+		if err != nil {
+			return nil, err
+		}
+		// TODO : helper function getLatestLSN() to get scan the segment for record and get lates lsn
+		lsn = getLatestLSN(*activeSeg)
+	case false:
+		// no segment file exists create new one
+		segID = 1
+		// lsn increments after append
+		lsn = 0
+		activeSeg, err = createSegment(dir, segID)
+		if err != nil {
+			return nil, err
+		}
+		// pointer to new segmentHeader
+		segHead := newSegmentHeader(walVersion, segID)
+
+		segHeadBytes := encodeSegmentHeader(*segHead)
+		//writing header to the segment file -> exactly one header per segment file
+		bytesWritten, err := activeSeg.file.Write(segHeadBytes)
+		if err != nil {
+			return nil, err
+		}
+		if bytesWritten != segmentHeaderByteSize {
+			return nil, fmt.Errorf("Expected %d bytes segment header, got %d bytes written", segmentHeaderByteSize, bytesWritten)
+		}
+		//update the current size of the segment file
+		activeSeg.currentSize += uint64(bytesWritten)
+	}
+	return &WAL{
+		dir:           dir,
+		activeSegment: activeSeg,
+		lsn:           lsn,
+		segID:         segID,
+		syncPolicy:    sp,
+	}, nil
+}
+
+// helper function to scan segment file and return latest (highest) segId
+func getLatestSegmentID(dir string) (uint64, bool, error) {
+	var segId uint64
+	var found bool
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return 0, false, err
+	}
+	for _, entry := range entries {
+		fileName := entry.Name()
+		if !entry.IsDir() && strings.HasSuffix(fileName, ".waldrky") {
+			numStr := strings.TrimSuffix(fileName, ".waldrky")
+			id, err := strconv.ParseUint(numStr, 10, 64)
+			if err != nil {
+				return 0, false, err
+			}
+			if !found || id > segId {
+				segId = id
+			}
+			found = true
+		}
+	}
+	return segId, found, nil
+}
+
+func (wal *WAL) Append() error {
+
+	return nil
+}
